f24a3bfd869387ff61e8aa06b3253cd51c01805d
Matthew Parnham <parnham@chapman.edu>
2018-12-01 00:21:41 -0800

    OMFG YEET

--- Server.java
+++ Server.java
@@ -1,36 +1,46 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class Server {
+    public static void main(String[] args) throws IOException {
 
-  public static void main(String[] args){
-    int portNumber = Integer.parseInt(args[0]);
-    System.out.println("Listening..");
-    try (
-        ServerSocket serverSocket = new ServerSocket(portNumber);
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out =
-            new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-            new InputStreamReader(clientSocket.getInputStream()));
-    ) {
-      String inputLine, outputLine;
+        if (args.length != 1) {
+            System.err.println("Usage: java Server <port number>");
+            System.exit(1);
+        }
 
-      // Initiate conversation with client
+        int portNumber = Integer.parseInt(args[0]);
 
-      KnockKnockProtocol kkp = new KnockKnockProtocol();
-      outputLine = kkp.processInput(null);
-      out.println(outputLine);
+        try (
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
 
-      while ((inputLine = in.readLine()) != null) {
-          outputLine = kkp.processInput(inputLine);
-          out.println(outputLine);
-          if (outputLine.equals("Bye."))
-              break;
-      }
+            String inputLine, outputLine;
+            Scanner k = new Scanner(System.in);
+
+            // Initiate conversation with client
+            System.out.println("Client has connected to server.");
+            outputLine = "Connected to Server.";
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+              System.out.println("Client: " + inputLine);
+              System.out.println("Message:");
+                outputLine = k.nextLine();
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
     }
-    catch(IOException e) {
-      System.out.println("IOException");
-    }
-  }
 }
